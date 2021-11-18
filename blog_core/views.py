a23f0b83284038730839a22f308254a977456870
Vlad <vladlapkovsky@gmail.com>
2021-11-18 18:07:34 +0300

    Add comment processing to the templates and views

--- blog_core/views.py
+++ blog_core/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 
-from blog_core.models import Post
-
+from blog_core.models import Post, Comment
+from django.db.models import Count
 
 def show_home(request):
     path = 'blog_core/home.html'
-    posts = Post.objects.all()
+    posts = Post.objects.all().annotate(Count('comment')).order_by('-published')
     context = {'posts': posts}
     return render(
         request=request,
@@ -17,7 +17,8 @@ def show_home(request):
 def show_post(request, post_slug):
     path = 'blog_core/post.html'
     post = get_object_or_404(Post, slug=post_slug)
-    context = {'post': post}
+    post_comments = Comment.objects.filter(post=post.id).order_by('published')
+    context = {'post': post, 'comments': post_comments}
     return render(
         request=request,
         template_name=path,

1ec6a3c9e79f97780eec8f28ee526567079cc181
Vlad <vladlapkovsky@gmail.com>
2021-11-18 15:46:17 +0300

    Add templates for main and post pages

--- blog_core/views.py
+++ blog_core/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from blog_core.models import Post
 
 
 def show_home(request):
-    return HttpResponse("There will be posts here")
+    path = 'blog_core/home.html'
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(
+        request=request,
+        template_name=path,
+        context=context,
+    )
 
 
-def show_post(request):
-    return HttpResponse("There will be post here")
+def show_post(request, post_slug):
+    path = 'blog_core/post.html'
+    post = get_object_or_404(Post, slug=post_slug)
+    context = {'post': post}
+    return render(
+        request=request,
+        template_name=path,
+        context=context,
+    )

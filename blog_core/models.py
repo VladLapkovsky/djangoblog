fcb7aaf5372df382b34219ca54da1c5fab8401e7
Vlad <vladlapkovsky@gmail.com>
2021-11-19 12:00:17 +0300

    Add multiple changes to the templates
    Add get_absolute_url func in Post model for slugs.
    Add filters to the templates.
    Change style.css for a tag.

--- blog_core/models.py
+++ blog_core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 from users.models import CustomUser
 
@@ -18,6 +19,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_slug': self.slug})
 
     class Meta:
         verbose_name_plural = 'posts'

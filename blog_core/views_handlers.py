f3457dc3d903145f0f1739a0e5808ccf2da1c12b
Vlad <vladlapkovsky@gmail.com>
2021-11-26 15:50:43 +0300

    Add views_handlers.py

---
+++ blog_core/views_handlers.py
@@ -0,0 +1,13 @@
+"""This module provides handlers for the blog_core views."""
+
+
+def get_slug_from_title(title: str) -> str:
+    """Reformat post title to post slug".
+
+    Args:
+        title (str): post title
+
+    Returns:
+        post slug
+    """
+    return title.strip('*!., ').lower().replace(' ', '-')
